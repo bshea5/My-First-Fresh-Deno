cc2bd4e6f9530800f59c01882b28b49e44dd51b8
Brandon Shea <bsshea5@gmail.com>
2022-11-21 12:56:02 -0800

    kid napping, so reviewing fresh example I did a bit back

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,16 +6,28 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+      <div>
+        <h1>Fresh Example here:</h1>
+        <img
+          src="/logo.svg"
+          height="100px"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class={tw`my-6`}>
+          Welcome to `fresh`. Try update this message in the ./routes/index.tsx
+          file, and refresh.
+        </p>
+        <Counter start={3} />
+      </div>
+      <div>
+        <h1>
+          Pokemon found here:{" "}
+          <a class={tw`text-blue-500`} href={`pokemon`}>
+            <span>poke</span>
+            <span class={tw`text-red-500`}>mon</span>
+          </a>
+        </h1>
+      </div>
     </div>
   );
 }
